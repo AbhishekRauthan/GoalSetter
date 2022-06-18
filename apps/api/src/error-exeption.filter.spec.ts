10f7a9c7e72c1d792c5ac823aeda119857b62711
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2022-06-18 16:03:50 +0530

    added Http Exeption handling and its test file

---
+++ apps/api/src/error-exeption.filter.spec.ts
@@ -0,0 +1,7 @@
+import { ErrorExeptionFilter } from './error-exeption.filter';
+
+describe('ErrorExeptionFilter', () => {
+  it('should be defined', () => {
+    expect(new ErrorExeptionFilter()).toBeDefined();
+  });
+});
