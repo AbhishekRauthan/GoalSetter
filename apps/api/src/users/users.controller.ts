2b6371d1dc9e710a5ebfe27993058770b9a46bd5
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2022-08-18 17:42:06 +0530

    The following was done:
    	- added service functions to handle connection to users databse
    	- added logic to handle user registeration at POST /users

--- apps/api/src/users/users.controller.ts
+++ apps/api/src/users/users.controller.ts
@@ -1,11 +1,22 @@
-import { Body, Controller, HttpException, HttpStatus, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpException,
+  HttpStatus,
+  Post,
+  Res,
+} from '@nestjs/common';
 import { Response } from 'express';
 import { registerUserBody } from './types';
+import { UsersService } from './users.service';
+import { genSalt, hash } from 'bcryptjs';
 
 @Controller('users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Post()
-  registerUser(@Body() body: registerUserBody, @Res() res: Response) {
+  async registerUser(@Body() body: registerUserBody, @Res() res: Response) {
     const { email, name, password } = body;
     if (!email || !name || !password) {
       throw new HttpException(
@@ -16,9 +27,40 @@ export class UsersController {
         HttpStatus.BAD_REQUEST
       );
     }
-    res.status(HttpStatus.OK).json({
-      message: 'POST / WORKING',
-    });
+
+    const userExist = await this.usersService.getUser(email);
+
+    if (userExist) {
+      throw new HttpException(
+        {
+          status: HttpStatus.FOUND,
+          error: 'User already exists',
+        },
+        HttpStatus.FOUND
+      );
+    }
+    const salt = await genSalt();
+    const hashedPassword = await hash(password, salt);
+    const user = await this.usersService.createUser(
+      name,
+      email,
+      hashedPassword
+    );
+    if (user) {
+      res.status(HttpStatus.OK).json({
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+      });
+    } else {
+      throw new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          error: 'Invalid user data',
+        },
+        HttpStatus.BAD_REQUEST
+      );
+    }
   }
 
   @Post('login')
--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -1,4 +1,19 @@
+import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { UserDocument, Users } from './users.schema';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel(Users.name) private userModel: Model<UserDocument>
+  ) {}
+
+  async getUser(email: string) {
+    return await this.userModel.findOne({ email });
+  }
+
+  async createUser(name: string, email: string, password: string) {
+    return await this.userModel.create({ email, name, password });
+  }
+}
