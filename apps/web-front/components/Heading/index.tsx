cc97868c5cb3d8be291a717ff3f8c430e8a6de9a
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2022-08-28 19:09:11 +0530

    added primary and secondary headings

---
+++ apps/web-front/components/Heading/index.tsx
@@ -0,0 +1,45 @@
+import {
+  HeadingProps as ChakraHeadingProps,
+  Heading,
+  Icon,
+} from '@chakra-ui/react';
+import { FC, ReactNode } from 'react';
+import { IconType } from 'react-icons/lib';
+
+interface HeadingProp extends ChakraHeadingProps {
+  children: ReactNode;
+  icon?: IconType;
+}
+
+export const PrimaryHeading: FC<HeadingProp> = ({
+  children,
+  icon,
+  ...rest
+}) => {
+  return (
+    <Heading
+      as="h1"
+      display="flex"
+      justifyContent="center"
+      textTransform="capitalize"
+      fontSize={{ base: '5xl' }}
+      {...rest}
+    >
+      <Icon as={icon} marginRight="3" />
+      {children}
+    </Heading>
+  );
+};
+
+export const SecondaryHeading: FC<HeadingProp> = ({ children, ...rest }) => {
+  return (
+    <Heading
+      as="h2"
+      color="#828282"
+      fontWeight="semibold"
+      {...rest}
+    >
+      {children}
+    </Heading>
+  );
+};
