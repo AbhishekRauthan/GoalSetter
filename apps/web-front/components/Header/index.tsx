6e5f76cec1bccaa50626cd3ecfee5f02824e84e9
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2022-08-31 15:13:45 +0530

    The following was done:
    	- utilized custom hook to get isUser value.
    	- based on isUser render logout button that on click calls reset from useAuthStore.
    	- or renders login and register buttons and menu.

--- apps/web-front/components/Header/index.tsx
+++ apps/web-front/components/Header/index.tsx
@@ -1,20 +1,27 @@
 import Link from 'next/link';
 import {
   Box,
+  Button,
   Flex,
   HStack,
+  Icon,
   IconButton,
   Menu,
   MenuButton,
   MenuItem,
   MenuList,
 } from '@chakra-ui/react';
-import { FaSignInAlt, FaUser } from 'react-icons/fa';
+import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa';
 import { HiMenuAlt3 } from 'react-icons/hi';
 import { LogoContainer } from './styles';
 import HeadLink from './HeadLink';
+import isUserShown from 'apps/web-front/hooks/isUserShown';
+import { useAuthStore } from 'apps/web-front/feature/store';
 
 const Header = () => {
+  const { isUser } = isUserShown();
+  const { reset } = useAuthStore();
+
   return (
     <Flex
       justifyContent="space-between"
@@ -34,36 +41,54 @@ const Header = () => {
       >
         <Link href="/">GoalSetter</Link>
       </LogoContainer>
-      <HStack
-        as="ul"
-        spacing="4"
-        display={{ base: 'none', md: 'flex' }}
-        justifyContent="space-between"
-        alignItems="center"
-        listStyleType="none"
-        color="black"
-      >
-        <HeadLink btnTxt="Login" href="/login" icon={FaSignInAlt} />
-        <HeadLink btnTxt="Register" href="/register" icon={FaUser} />
-      </HStack>
-      <Box display={{ base: 'block', md: 'none' }}>
-        <Menu isLazy>
-          <MenuButton
-            as={IconButton}
-            icon={<HiMenuAlt3 />}
-            aria-label="menu"
-            variant="ghost"
-          />
-          <MenuList listStyleType="none" minW="max-content">
-            <MenuItem width="max-content">
-              <HeadLink btnTxt="Login" href="/login" icon={FaSignInAlt} />
-            </MenuItem>
-            <MenuItem>
-              <HeadLink btnTxt="Register" href="/register" icon={FaUser} />
-            </MenuItem>
-          </MenuList>
-        </Menu>
-      </Box>
+      {isUser ? (
+        <>
+          <Button
+            variant="link"
+            leftIcon={<Icon as={FaSignOutAlt} />}
+            color="black"
+            textTransform="capitalize"
+            size={{ base: 'xs', lg: 'lg' }}
+            fontWeight="medium"
+            onClick={() => reset()}
+          >
+            logout
+          </Button>
+        </>
+      ) : (
+        <>
+          <HStack
+            as="ul"
+            spacing="4"
+            display={{ base: 'none', md: 'flex' }}
+            justifyContent="space-between"
+            alignItems="center"
+            listStyleType="none"
+            color="black"
+          >
+            <HeadLink btnTxt="Login" href="/login" icon={FaSignInAlt} />
+            <HeadLink btnTxt="Register" href="/register" icon={FaUser} />
+          </HStack>
+          <Box display={{ base: 'block', md: 'none' }}>
+            <Menu isLazy>
+              <MenuButton
+                as={IconButton}
+                icon={<HiMenuAlt3 />}
+                aria-label="menu"
+                variant="ghost"
+              />
+              <MenuList listStyleType="none" minW="max-content">
+                <MenuItem width="max-content">
+                  <HeadLink btnTxt="Login" href="/login" icon={FaSignInAlt} />
+                </MenuItem>
+                <MenuItem>
+                  <HeadLink btnTxt="Register" href="/register" icon={FaUser} />
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </Box>
+        </>
+      )}
     </Flex>
   );
 };
