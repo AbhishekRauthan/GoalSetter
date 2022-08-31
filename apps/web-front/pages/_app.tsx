0795327e0b061702d0b4cca8809450fa595b4688
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2022-08-31 16:26:06 +0530

    The following was done:
    	- added logic to to check if user had previously logged in.
    	- if yes, then set user from localStorage.
    	- if no, go to /login route.

--- apps/web-front/pages/_app.tsx
+++ apps/web-front/pages/_app.tsx
@@ -3,8 +3,19 @@ import Header from '../components/Header';
 import { ChakraProvider } from '@chakra-ui/react';
 import Head from 'next/head';
 import { theme } from '../theme';
+import { useEffect } from 'react';
+import { useAuthStore } from '../feature/store';
+import router from 'next/router';
 
 function CustomApp({ Component, pageProps }: AppProps) {
+  const { setUser } = useAuthStore();
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const localUser = localStorage.getItem('user_token');
+      localUser ? setUser(JSON.parse(localUser)) : router.push('/login');
+    }
+  }, []);
+
   return (
     <>
       <Head>
