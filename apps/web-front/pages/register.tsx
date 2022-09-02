5fff3d87727861441d54dbcd808d39bd1445df64
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2022-09-02 17:20:39 +0530

    impleamented FormCon and CenterCon wrappers

--- apps/web-front/pages/register.tsx
+++ apps/web-front/pages/register.tsx
@@ -1,37 +1,26 @@
-import { Box, VStack } from '@chakra-ui/react';
-import { useState } from 'react';
+import { Box } from '@chakra-ui/react';
 import { FaUser } from 'react-icons/fa';
 import { PrimaryBtn } from '../components/Button';
+import { CenterCon, FormCon } from '../components/Container';
 import { PrimaryHeading, SecondaryHeading } from '../components/Heading';
 import { Input, PasswordInput } from '../components/Input';
 
 const Register = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: '',
-    password2: '',
-  });
   return (
     <>
-      <VStack
-        alignItems="center"
-        justifyContent="center"
-        paddingTop="16"
-        spacing="5"
-      >
+      <CenterCon>
         <Box>
           <PrimaryHeading icon={FaUser}>Register</PrimaryHeading>
           <SecondaryHeading>Please create an account</SecondaryHeading>
         </Box>
-        <VStack as="form" paddingTop="7" spacing="8">
+        <FormCon>
           <Input type="name">Name</Input>
           <Input type="email">Email</Input>
           <PasswordInput>password</PasswordInput>
           <PasswordInput>confirm password</PasswordInput>
           <PrimaryBtn>Register</PrimaryBtn>
-        </VStack>
-      </VStack>
+        </FormCon>
+      </CenterCon>
     </>
   );
 };
