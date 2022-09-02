30a6894cebe4dd6c1143f7df22c7f20949c22ea1
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2022-09-02 17:56:41 +0530

    The following changes were done:
    	- in onSubmit added a try catch block.
    	- in try block used setUser and used router to navigate to / route
    	- added a toast for error handling in catch block.
    	- wrapped whole component with CenterCon
    	- conditionally render login form and a heading to tell they are already logged in.

--- apps/web-front/pages/login.tsx
+++ apps/web-front/pages/login.tsx
@@ -1,52 +1,64 @@
-import { Box, VStack } from '@chakra-ui/react';
+import { Box, useToast } from '@chakra-ui/react';
 import { useRef } from 'react';
 import { FaSignInAlt } from 'react-icons/fa';
 import { PrimaryBtn } from '../components/Button';
 import { PrimaryHeading, SecondaryHeading } from '../components/Heading';
 import { Input, PasswordInput } from '../components/Input';
 import axios from 'axios';
-import { FormCon } from '../components/Container';
+import router from 'next/router';
+import { CenterCon, FormCon } from '../components/Container';
+import { useAuthStore } from '../feature/store';
+import isUserShown from '../hooks/isUserShown';
 
 const Login = () => {
+  const { isUser } = isUserShown();
   const emailRef = useRef<HTMLInputElement>();
   const pwrdRef = useRef<HTMLInputElement>();
-
+  const { setUser } = useAuthStore();
+  const toast = useToast();
   async function onSubmit() {
     const email = emailRef.current.value;
     const password = pwrdRef.current.value;
 
-    const response = await axios.post('/api/users/login', {
-      email,
-      password,
-    });
-    localStorage.setItem('user_token', JSON.stringify(response.data));
-
-    const user = localStorage.getItem('user_token');
-    console.log(JSON.parse(user));
-    
+    try {
+      const response = await axios.post('/api/users/login', {
+        email,
+        password,
+      });
+      setUser(response.data);
+      router.push('/');
+    } catch (error) {
+      toast({
+        position: 'bottom',
+        status: 'error',
+        isClosable: true,
+        title: 'Error! Unable to Login',
+      });
+    }
   }
 
   return (
-    <>
-      <VStack
-        alignItems="center"
-        justifyContent="center"
-        paddingTop="16"
-        spacing="5"
-      >
-        <Box>
-          <PrimaryHeading icon={FaSignInAlt}>login</PrimaryHeading>
-          <SecondaryHeading>Login and start setting goals</SecondaryHeading>
-        </Box>
-        <FormCon>
-          <Input type="email" ref={emailRef}>
-            email
-          </Input>
-          <PasswordInput ref={pwrdRef}>password</PasswordInput>
-          <PrimaryBtn onClick={onSubmit}>Login</PrimaryBtn>
-        </FormCon>
-      </VStack>
-    </>
+    <CenterCon>
+      {isUser ? (
+        <>
+          <PrimaryHeading>Already logged in as a user</PrimaryHeading>
+        </>
+      ) : (
+        <>
+          <Box>
+            <PrimaryHeading icon={FaSignInAlt}>login</PrimaryHeading>
+            <SecondaryHeading>Login and start setting goals</SecondaryHeading>
+          </Box>
+          <FormCon>
+            <Input type="email" ref={emailRef}>
+              email
+            </Input>
+            <PasswordInput ref={pwrdRef}>password</PasswordInput>
+            <PrimaryBtn onClick={onSubmit}>Login</PrimaryBtn>
+          </FormCon>
+        </>
+      )}
+    </CenterCon>
   );
 };
 
