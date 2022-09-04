e11a76c3b6f7e82d9e54328513c63161276550b1
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2022-09-04 18:19:06 +0530

    The following was done:
    	- added functionality to change border color of confirm password if it matches password or not.
    	- added functionality to register user and show error message based on error response

--- apps/web-front/pages/register.tsx
+++ apps/web-front/pages/register.tsx
@@ -1,11 +1,55 @@
-import { Box } from '@chakra-ui/react';
+import { Box, useToast } from '@chakra-ui/react';
+import axios from 'axios';
+import Router from 'next/router';
+import { useRef, useState } from 'react';
 import { FaUser } from 'react-icons/fa';
 import { PrimaryBtn } from '../components/Button';
 import { CenterCon, FormCon } from '../components/Container';
 import { PrimaryHeading, SecondaryHeading } from '../components/Heading';
 import { Input, PasswordInput } from '../components/Input';
+import { useAuthStore } from '../feature/store';
 
 const Register = () => {
+  const [isEqualPass, setIsEqualPass] = useState(false);
+  const nameRef = useRef<HTMLInputElement>();
+  const emailRef = useRef<HTMLInputElement>();
+  const passRef = useRef<HTMLInputElement>();
+  const conPassRef = useRef<HTMLInputElement>();
+  const toast = useToast();
+  const { setUser } = useAuthStore();
+  const checkPasswords = () => {
+    if (passRef.current.value === conPassRef.current.value) {
+      setIsEqualPass(true);
+    } else {
+      setIsEqualPass(false);
+    }
+  };
+  const onSubmit = async () => {
+    const name = nameRef.current.value;
+    const email = emailRef.current.value;
+    const password = passRef.current.value;
+
+    await axios
+      .post('/api/users', {
+        email,
+        name,
+        password,
+      })
+      .then((response) => {
+        setUser(response.data);
+        Router.push('/');
+      })
+      .catch((err) => {
+        console.log(err);
+
+        toast({
+          position: 'top',
+          status: 'error',
+          isClosable: true,
+          title: err.response.data.error,
+        });
+      });
+  };
   return (
     <>
       <CenterCon>
@@ -14,11 +58,24 @@ const Register = () => {
           <SecondaryHeading>Please create an account</SecondaryHeading>
         </Box>
         <FormCon>
-          <Input type="name">Name</Input>
-          <Input type="email">Email</Input>
-          <PasswordInput>password</PasswordInput>
-          <PasswordInput>confirm password</PasswordInput>
-          <PrimaryBtn>Register</PrimaryBtn>
+          <Input type="text" ref={nameRef}>
+            Name
+          </Input>
+          <Input type="email" ref={emailRef}>
+            Email
+          </Input>
+          <PasswordInput ref={passRef} onChange={checkPasswords}>
+            password
+          </PasswordInput>
+          <PasswordInput
+            ref={conPassRef}
+            onChange={checkPasswords}
+            focusBorderColor={isEqualPass ? 'green.500' : 'red.500'}
+            borderColor={isEqualPass ? 'green.500' : 'red.500'}
+          >
+            confirm password
+          </PasswordInput>
+          <PrimaryBtn onClick={onSubmit}>Register</PrimaryBtn>
         </FormCon>
       </CenterCon>
     </>
