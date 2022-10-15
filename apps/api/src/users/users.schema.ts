3b728e2d8d653acf6190f22182314d0a9b56b1b6
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2022-10-15 12:37:00 +0530

    impleamented User schema as graphql type

--- apps/api/src/users/users.schema.ts
+++ apps/api/src/users/users.schema.ts
@@ -1,15 +1,22 @@
+import { Field, ObjectType } from '@nestjs/graphql';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Document, Schema as MongooseSchema } from 'mongoose';
 
 export type UserDocument = Users & Document;
 
 @Schema({ timestamps: true })
+@ObjectType()
 export class Users {
+  @Field(() => String)
+  _id: MongooseSchema.Types.ObjectId;
   @Prop({ required: [true, 'Please add a name'], type: String })
+  @Field(() => String, { description: 'User name' })
   name: string;
   @Prop({ required: [true, 'Please add an email'], unique: true, type: String })
+  @Field(() => String, { description: 'User email' })
   email: string;
   @Prop({ required: [true, 'Please add a password'], type: String })
+  @Field(() => String, { description: 'User password' })
   password: string;
 }
 
