cd8ea84d73849baf28235dd24fbda3c557862354
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2022-10-31 13:45:08 +0530

    The following was done:
    	- deleted user controller spec file.
    	- created user resolver test file and implemented test for user registration.
    	- implemented and tested user registration inside graphql user resolver.

--- apps/api/src/users/users.resolver.spec.ts
+++ apps/api/src/users/users.resolver.spec.ts
@@ -1,18 +1,88 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersResolver } from './users.resolver';
+import { UsersService } from './users.service';
+import { JwtService } from '@nestjs/jwt';
+
+type MockType<T> = {
+  [P in keyof T]?: jest.Mock<{}>;
+};
 
 describe('UsersResolver', () => {
   let resolver: UsersResolver;
-
+  const userServiceMock: MockType<UsersService> = {
+    createUser: jest.fn(),
+    getUser: jest.fn(),
+  };
+  const jwtServiceMock: MockType<JwtService> = {
+    sign: jest.fn(),
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersResolver],
+      providers: [
+        UsersResolver,
+        {
+          provide: UsersService,
+          useValue: userServiceMock,
+        },
+        {
+          provide: JwtService,
+          useValue: jwtServiceMock,
+        },
+      ],
     }).compile();
 
     resolver = module.get<UsersResolver>(UsersResolver);
   });
+  describe('Testing registerUser', () => {
+    it('should throw CONFLICT Exception', async () => {
+      const registrationDetails = {
+        email: 'test@example.com',
+        name: 'test name',
+        password: 'test_password',
+      };
+      try {
+        userServiceMock.getUser.mockImplementation(() =>
+          Promise.resolve(registrationDetails)
+        );
+        await resolver.registerUser(registrationDetails);
+      } catch (error) {
+        expect(error.response.message).toBe('User already exists');
+      }
+    });
 
-  it('should be defined', () => {
-    expect(resolver).toBeDefined();
+    it('should create and return user details', async () => {
+      const registrationDetails = {
+        email: 'test@example.com',
+        name: 'test name',
+        password: 'test_password',
+      };
+      userServiceMock.getUser.mockImplementation(() => Promise.resolve(null));
+
+      userServiceMock.createUser.mockImplementation(() =>
+        Promise.resolve({
+          id: '1',
+          ...registrationDetails,
+        })
+      );
+      jwtServiceMock.sign.mockImplementation(() => 'Some gibberish token');
+      const userDeatils = await resolver.registerUser(registrationDetails);
+      expect(userDeatils).toEqual({ id: '1', ...registrationDetails });
+    });
+
+    it('should fail to create user and throw NotImplemented Exception', async () => {
+      const registrationDetails = {
+        email: 'test@example.com',
+        name: 'test name',
+        password: 'test_password',
+      };
+      userServiceMock.getUser.mockImplementation(() => Promise.resolve(null));
+
+      userServiceMock.createUser.mockImplementation(() => Promise.reject(null));
+      try {
+        await resolver.registerUser(registrationDetails);
+      } catch (error) {
+        expect(error.response.message).toBe('Unable to create user');
+      }
+    });
   });
 });
--- apps/api/src/users/users.resolver.ts
+++ apps/api/src/users/users.resolver.ts
@@ -1,9 +1,46 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import {
+  BadRequestException,
+  ConflictException,
+  NotImplementedException,
+  UsePipes,
+} from '@nestjs/common';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { JwtService } from '@nestjs/jwt';
+import { genSalt, hash } from 'bcryptjs';
+import { UserModel } from './model/user.model';
+import { UserRegisterModel } from './model/userRegister.input';
+import { Users } from './users.schema';
+import { UsersService } from './users.service';
+import { ValidationPipe } from '../pipe/validation.pipe';
 
-@Resolver()
+@Resolver(() => Users)
 export class UsersResolver {
-  @Query(() => String)
-  sayUser(): string {
-    return 'Hello User!';
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService
+  ) {}
+
+  @Mutation((returns) => UserModel)
+  @UsePipes(new ValidationPipe())
+  async registerUser(
+    @Args('userRegisterModel')
+    userRegisterModel: UserRegisterModel
+  ) {
+    const { email, name, password } = userRegisterModel;
+
+    const userExist = await this.usersService.getUser(email);
+
+    if (userExist) {
+      throw new ConflictException('User already exists');
+    }
+
+    const salt = await genSalt();
+    const hashedPassword = await hash(password, salt);
+    const user = await this.usersService
+      .createUser(name, email, hashedPassword)
+      .catch(() => {
+        throw new NotImplementedException('Unable to create user');
+      });
+    return user;
   }
 }
